bb7e591d1188e5f0c1035d987365902a4b14125f
Nick Donohue <ndonohue@gmail.com>
2014-12-04 16:42:50 -0500

    more robust integration

--- app/controllers/api/v1/integration_controller.rb
+++ app/controllers/api/v1/integration_controller.rb
@@ -3,12 +3,20 @@ class Api::V1::IntegrationController < ApplicationController
 
   def readings
     begin
-      @readings = Reading.create(permitted_params[:readings])
+      reading_data = permitted_params[:readings]
+
+      if reading_data.nil? or reading_data.empty?
+        throw Exception.new 'Readings must not be empty'
+      end
+
+      @readings = Reading.create(reading_data)
+
       if @readings.to_a.all? { |t| t.valid? }
         render json: { message: 'Accepted' }, status: 202
       else
         render json: { errors: @readings.errors.full_messages }, status: 400
       end
+
     rescue => e
       render json: { message: 'There was a problem parsing the data', exception: e.to_s }, status: 400
     end
