39a4ea545c26660709256c0e48e2d930685245f5
Nick Donohue <ndonohue@gmail.com>
2014-12-04 23:45:34 -0500

    accept single item on same endpoint

--- app/controllers/api/v1/integration_controller.rb
+++ app/controllers/api/v1/integration_controller.rb
@@ -3,26 +3,37 @@ class Api::V1::IntegrationController < ApplicationController
 
   def readings
     begin
-      reading_data = permitted_params[:readings]
+      my_params = permitted_params
+      reading_data = my_params[:readings]
 
       if reading_data.nil? or reading_data.empty?
-        throw Exception.new 'Readings must not be empty'
-      end
+        reading_data = my_params
 
-      @readings = Reading.create(reading_data)
-
-      if @readings.to_a.all? { |t| t.valid? }
-        render json: { message: 'Accepted' }, status: 202
+        @reading = Reading.create(reading_data)
+        if @reading.valid?
+          render json: {message: 'Created'}, status: 201
+        else
+          render json: {errors: @reading.errors.full_messages}, status: 400
+        end
       else
-        render json: { errors: @readings.errors.full_messages }, status: 400
+        @readings = Reading.create(reading_data)
+        if @readings.to_a.all? { |t| t.valid? }
+          render json: {message: 'Accepted'}, status: 202
+        else
+          render json: {errors: @readings.errors.full_messages}, status: 400
+        end
       end
 
     rescue => e
-      render json: { message: 'There was a problem parsing the data', exception: e.to_s }, status: 400
+      render json: {message: 'There was a problem parsing the data', exception: e.to_s}, status: 400
     end
   end
 
   def permitted_params
-    params.permit(readings: [:account_number, :read_at, :read_type, :usage])
+    params.permit(:account_number,
+                  :read_at,
+                  :read_type,
+                  :usage,
+                  readings: [:account_number, :read_at, :read_type, :usage])
   end
 end
